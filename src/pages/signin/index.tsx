83230512c7665dea38f4f25dc572abeee4283220
RootCho <whgpwls210@naver.com>
2023-05-14 20:53:45 +0900

    [Feat] 카카오 로그인 인증 코드 수정

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -3,15 +3,13 @@ import Badge from "../../../public/assets/images/splash_badge.svg";
 import Kakao from "../../../public/assets/images/kakao_login.svg";
 import Google from "../../../public/assets/images/google_logo.svg";
 import Apple from "../../../public/assets/images/apple_logo.svg";
-import { signIn, useSession } from "next-auth/react";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 const SignIn = () => {
-  const sesstion = useSession();
-  console.log(sesstion);
-  const kakaoSignIn = () => {
-    window.Kakao.Auth.authorize({
-      redirectUri: "http://localhost:3000/signin",
-    });
-  };
+  const session = useSession();
+  console.log(session);
+
   return (
     <div id={styles.signIn}>
       <div className={styles.contentBox}>
@@ -22,12 +20,13 @@ const SignIn = () => {
         </h2>
         <p>적은 비용으로 어디에도 없던 숨은 비법을 즐기세요!</p>
         <Badge className={styles.badge} />
-        <Kakao className={styles.kakao} onClick={kakaoSignIn} />
+        <Kakao className={styles.kakao} onClick={() => signIn("kakao")} />
         <span className={styles.or}>또는</span>
         <section className={styles.logoWrap}>
           <Google className={styles.google} onClick={() => signIn("google")} />
           <Apple className={styles.apple} onClick={() => signIn("apple")} />
         </section>
+        {/* <button onClick={() => signOut()}>로그아웃</button> */}
       </div>
     </div>
   );
