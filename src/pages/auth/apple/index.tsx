d0a28350988e4e1f7ef067b6d8215746f24b1f80
OliveCho <whgpwls210@naver.com>
2023-06-14 17:07:24 +0900

    [Test] apple signin

--- src/pages/auth/apple/index.tsx
+++ src/pages/auth/apple/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
 
@@ -9,6 +10,19 @@ const AppleAuth = () => {
     "refreshToken",
   ]);
 
+  //https://appleid.apple.com/auth/token
+
+  // const res = await axios.post(
+  //     `https://appleid.apple.com/auth/token`,
+  //     {
+  //       code: query?.code,
+  //       grant_type: "authorization_code",
+  //       client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  //       client_secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+  //       redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
+  //     },
+  //     { headers: { "content-type": "application/x-www-form-urlencoded" } }
+  //   );
   return <div></div>;
 };
 
