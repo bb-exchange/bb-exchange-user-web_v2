769cde31e9a1401705c0399993e052c2408d3d1a
MINHEE <38336567+minnie-J@users.noreply.github.com>
2024-01-21 20:17:23 +0900

    [BB-806] 작성가이드 페이지 (#124)
    * [Feat] 페이지 레이아웃 구성
    
    * [Feat] 이미지 교체
    
    * [Refactor] 웹 페이지 컴포넌트 분리
    
    * [Feat] 모바일웹용 이미지 추가
    
    * [Feat] 모바일 페이지 컴포넌트
    
    * [Refactor] 스타일 파일 정리
    
    * [Feat] 링크 처리
    
    * [Feat] 접속 기기 파악 후 화면 분기 처리

---
+++ src/pages/guide/index.tsx
@@ -0,0 +1,28 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+
+import DesktopPage from ".src/components/guide/desktop";
+import MobilePage from ".src/components/guide/mobile";
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const userAgent = context.req.headers["user-agent"];
+
+  return {
+    props: {
+      isMobile: !!userAgent?.includes("Mobile"),
+    },
+  };
+};
+
+const GuidePage = ({
+  isMobile,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return <>{isMobile ? <MobilePage /> : <DesktopPage />}</>;
+};
+
+export default GuidePage;
