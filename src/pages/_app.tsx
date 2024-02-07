f5b2773ea6a5259ea088a695f7f35e97f2425c82
MINHEE <38336567+minnie-J@users.noreply.github.com>
2024-02-08 00:54:19 +0900

    [BB-526] 페이지별 동일 오픈그래프 이미지가 나오는 현상 수정 (#148)
    * [Fix] 페이지별 동일 오픈그래프 이미지가 나오는 현상 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,14 +35,6 @@ export default function App({ Component, ...rest }: AppProps) {
   //tracking the current and previous page
   useEffect(() => storePathValues, [router.asPath]);
 
-  const [currentUrl, setCurrentUrl] = useState<string>();
-
-  useEffect(() => {
-    if (currentUrl != null) return;
-    const url = window.location.origin;
-    setCurrentUrl(url);
-  }, [currentUrl]);
-
   function storePathValues() {
     const storage = globalThis?.sessionStorage;
     if (!storage) return;
@@ -59,29 +51,6 @@ export default function App({ Component, ...rest }: AppProps) {
         <QueryClientProvider client={queryClient}>
           <Head>
             <title>비법거래소</title>
-            <meta property="og:title" content="비법거래소" />
-            <meta property="og:url" content={currentUrl} />
-            <meta
-              property="og:image"
-              content="/assets/images/og_image_service_kakao.png"
-            />
-            <meta
-              property="og:description"
-              content="제2의 월급, 비법거래소에서"
-            />
-
-            <meta name="twitter:card" content="summary" />
-            <meta property="twitter:domain" content={currentUrl} />
-            <meta property="twitter:url" content={currentUrl} />
-            <meta name="twitter:title" content="비법거래소" />
-            <meta
-              name="twitter:description"
-              content="제2의 월급, 비법거래소에서"
-            />
-            <meta
-              name="twitter:image"
-              content="/assets/images/og_image_service_x.png"
-            />
           </Head>
 
           <Layout pageProps={rest.pageProps}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from "react";
 import {
   GetServerSideProps,
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next";
+import Head from "next/head";
 import {
   DehydratedState,
   HydrationBoundary,
@@ -48,8 +50,36 @@ export const getServerSideProps: GetServerSideProps<{
 export default function Home({
   dehydratedState,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [currentUrl, setCurrentUrl] = useState<string>();
+
+  useEffect(() => {
+    if (currentUrl != null) return;
+    const url = window.location.origin;
+    setCurrentUrl(url);
+  }, [currentUrl]);
+
   return (
     <HydrationBoundary state={dehydratedState}>
+      <Head>
+        <meta property="og:title" content="비법거래소" />
+        <meta property="og:url" content={currentUrl} />
+        <meta
+          property="og:image"
+          content="/assets/images/og_image_service_kakao.png"
+        />
+        <meta property="og:description" content="제2의 월급, 비법거래소에서" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta property="twitter:domain" content={currentUrl} />
+        <meta property="twitter:url" content={currentUrl} />
+        <meta name="twitter:title" content="비법거래소" />
+        <meta name="twitter:description" content="제2의 월급, 비법거래소에서" />
+        <meta
+          name="twitter:image"
+          content="/assets/images/og_image_service_x.png"
+        />
+      </Head>
+
       <Lastest />
     </HydrationBoundary>
   );
