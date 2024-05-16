95a9d61521defb67e3da9a468a134acdfc1eabd3
injulme <injulme0309@naver.com>
2024-05-16 16:49:43 +0900

    test: test code 삭제
    #BB-1097

--- src/components/mypage/profSec.tsx
+++ src/components/mypage/profSec.tsx
@@ -75,7 +75,6 @@ export default function ProfSec() {
                 )}
               </p>
               <ChevronRtBlue />
-                test
 
               {/* <button
                 className={styles.detailBtn}
