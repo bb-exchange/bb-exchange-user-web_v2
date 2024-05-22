3bc799d588cee9a029458a3dda1c491a76274acd
injulme <injulme0309@naver.com>
2024-05-22 23:13:44 +0900

    feat: 마이페이지 포인트 전환 api 작업
    #BB-1097

--- src/api/mypage/settlement.ts
+++ src/api/mypage/settlement.ts
@@ -3,3 +3,7 @@ import { basicInstance } from "../instance";
 export const getActual = async () => {
   return await basicInstance.get("/v1/settlement/actual");
 };
+
+export const postProfitToPoint = async ({ amount }: { amount: number }) => {
+  return await basicInstance.post("/v1/settlement/profit-to-point", { amount });
+};
--- src/components/mypage/asset/pointPopup.tsx
+++ src/components/mypage/asset/pointPopup.tsx
@@ -2,9 +2,12 @@ import styles from "./pointPopup.module.scss";
 
 import { useState } from "react";
 
+import { useMutation } from "@tanstack/react-query";
 import cn from "classnames";
 
 import ChevronDn from "@assets/icons/ChevronDn.svg";
+
+import { postProfitToPoint } from "@api/mypage/settlement";
 
 import ContainedBtn from "@components/Buttons/ContainedBtn";
 import Popup from "@components/Popup";
@@ -13,11 +16,49 @@ type PointPopupProps = {
   visible?: boolean;
   onClose?: () => void;
 };
-const MONEY_SUFFIX = "원";
-const POINT_SUFFIX = "P";
+
+const maxValue = 65154;
 
 export default function PointPopup({ visible, onClose }: PointPopupProps) {
-  const [changePointValue, setChangePointValue] = useState("");
+  const [changePointValue, setChangePointValue] = useState(0);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const validInputValue = Number(e.target.value.replace(/[^0-9]/g, ""));
+
+    setChangePointValue(Number(validInputValue));
+  };
+
+  const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const validInputValue = Number(e.target.value.replaceAll(",", ""));
+
+    let result;
+    if (validInputValue >= maxValue) {
+      result = maxValue;
+    } else {
+      result = validInputValue;
+    }
+    setChangePointValue(result);
+  };
+
+  const addPoint = (count: number) => {
+    const point = Number(changePointValue);
+    let result;
+    if (point + count >= maxValue) {
+      result = maxValue;
+    } else {
+      result = point + count;
+    }
+    setChangePointValue(result);
+  };
+
+  const onCheckMaximum = () => {};
+
+  const { mutate: postPoint } = useMutation({
+    mutationFn: () => postProfitToPoint({ amount: changePointValue }),
+    onSuccess: () => {
+      console.log("success");
+    },
+  });
 
   return (
     <Popup title="포인트로 전환" visible={visible} onClose={onClose}>
@@ -34,14 +75,21 @@ export default function PointPopup({ visible, onClose }: PointPopupProps) {
           <input
             type="text"
             placeholder="얼마나 전환할까요?"
-            // onChange={(e) => setChangePointValue(e.target.value)}
-            value={changePointValue + MONEY_SUFFIX}
+            onChange={onChange}
+            value={changePointValue.toLocaleString()}
+            onBlur={onBlur}
           />
         </div>
         <div className={cn(styles.buttonWrapper, styles.mt12)}>
-          <button className={styles.flexibleLine}>+1,000원</button>
-          <button className={styles.flexibleLine}>+3,000원</button>
-          <button className={styles.flexibleLine}>+5,000원</button>
+          <button className={styles.flexibleLine} onClick={() => addPoint(1000)}>
+            +1,000원
+          </button>
+          <button className={styles.flexibleLine} onClick={() => addPoint(3000)}>
+            +3,000원
+          </button>
+          <button className={styles.flexibleLine} onClick={() => addPoint(5000)}>
+            +5,000원
+          </button>
         </div>
         <div style={{ textAlign: "center", margin: "20px 0" }}>
           <ChevronDn />
@@ -53,14 +101,15 @@ export default function PointPopup({ visible, onClose }: PointPopupProps) {
           <input
             type="text"
             placeholder="얼마나 전환할까요?"
-            // onChange={(e) => setChangePointValue(e.target.value)}
-            value={changePointValue + POINT_SUFFIX}
+            onChange={onChange}
+            value={changePointValue.toLocaleString()}
+            onBlur={onBlur}
           />
         </div>
         <span className="p2 color-primary1" style={{ margin: "8px 0", textAlign: "center" }}>
           <strong>1원</strong> 단위로 전환 가능해요.
         </span>
-        <ContainedBtn text="전환하기" disabled />
+        <ContainedBtn text="전환하기" disabled={changePointValue <= 0} onClick={postPoint} />
       </div>
     </Popup>
   );
