712182ae9a7ee7b93bbfcb222436fefdb5ca4766
injulme <injulme0309@naver.com>
2024-06-02 23:37:54 +0900

    feat: get banks api
    - 주석 추가

--- src/api/bank/api.ts
+++ src/api/bank/api.ts
@@ -1,5 +1,14 @@
 import { basicInstance } from "../instance";
 
+/**
+ * 유저 은행정보 불러오기 api
+ */
 export const getBankInfo = async () => {
   return await basicInstance.get("/v1/bankdetails");
+};
+/**
+ * 은행 목록 조회 api
+ */
+export const getBanks = async () => {
+  return await basicInstance.get("/v1/banks");
 };
