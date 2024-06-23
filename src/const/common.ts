e50b31bf9faae4b3d22c4439520fd7612fdc7410
injulme <injulme0309@naver.com>
2024-06-23 22:20:58 +0900

    fix: 알림 태그 텍스트 수정
    #BB-1278

--- src/const/common.ts
+++ src/const/common.ts
@@ -17,47 +17,47 @@ export type NotificationInteraction =
   | "DAILY_EVENT_ALL_DONE";
 export const NotificationTypeCode: CodeType<NotificationInteraction> = {
   ARTICLE_STOCK_LISTED: {
-    label: "게시글 상장",
+    label: "내 글 상장",
     value: "ARTICLE_STOCK_LISTED",
   },
   ARTICLE_STOCK_DELISTED: {
-    label: "게시글 상장 취소",
+    label: "내 글 상장 폐지",
     value: "ARTICLE_STOCK_DELISTED",
   },
   ARTICLE_NEW_COMMENT: {
-    label: "새로운 댓글",
+    label: "댓글",
     value: "ARTICLE_NEW_COMMENT",
   },
   ARTICLE_SALE_SUSPENDED_TEMP: {
-    label: "판매 일시 중지",
+    label: "일시 판매 중지",
     value: "ARTICLE_SALE_SUSPENDED_TEMP",
   },
   ARTICLE_SALE_SUSPENDED: {
-    label: "판매 중지",
+    label: "영구 판매 중지",
     value: "ARTICLE_SALE_SUSPENDED",
   },
   ARTICLE_REJECTED: {
-    label: "게시글 거절",
+    label: "비공개 처리",
     value: "ARTICLE_REJECTED",
   },
   PROFIT_SETTLEMENT_REQUEST_DONE: {
-    label: "수익 정산 요청 완료",
+    label: "출금신청",
     value: "PROFIT_SETTLEMENT_REQUEST_DONE",
   },
   PROFIT_SETTLEMENT_DONE: {
-    label: "수익 정산 완료",
+    label: "출금완료",
     value: "PROFIT_SETTLEMENT_DONE",
   },
   TAX_INVOICE_ISSUE_DONE: {
-    label: "세금계산서 발행 완료",
+    label: "세금계산서 발행",
     value: "TAX_INVOICE_ISSUE_DONE",
   },
   ARTICLE_LIKED: {
-    label: "게시글 좋아요",
+    label: "좋아요",
     value: "ARTICLE_LIKED",
   },
   COMMENT_LIKED: {
-    label: "댓글 좋아요",
+    label: "좋아요",
     value: "COMMENT_LIKED",
   },
   DAILY_EVENT_WRITE_ARTICLE_WITH_LIKES_DONE: {
