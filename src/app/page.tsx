5f1ef1c35835ab20e7e365d1de8180fe22ecf654
injulme <injulme0309@naver.com>
2024-08-25 20:52:56 +0900

    feat: toast 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import { toast } from "sonner";
+
 import { Flex, Grid, Section } from "@/shared/components/layouts";
+import { Button } from "@/shared/components/ui/button";
 import { Skeleton } from "@/shared/components/ui/skeleton";
+import { Toaster } from "@/shared/components/ui/toast";
 
 export default function Home() {
   return (
     <Section>
+      <Toaster />
+
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            position: "top-center",
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+      >
+        Show Toast
+      </Button>
       <Flex direction="column" gap="2">
         <Flex>
           <Skeleton height="60px" width="100%">
