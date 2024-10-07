fa1b1b0e3712940ffd4b9d9c7fda05320ddde536
injulme <injulme0309@naver.com>
2024-10-07 21:19:28 +0900

    feat: badge 컴포넌트 구현
    #BB-1442

--- src/shared/components/ui/badge/index.tsx
+++ src/shared/components/ui/badge/index.tsx
@@ -1,7 +1,40 @@
-import { Badge as RadixBadge } from '@radix-ui/themes';
+"use client";
 
-const Badge = RadixBadge;
+import * as React from "react";
 
-Badge.displayName = 'Badge';
+import "./badge.scss";
+import { Slot } from "@radix-ui/react-slot";
+import cn from "classnames";
+
+interface BadgeProps extends React.ComponentPropsWithoutRef<"span"> {
+  asChild?: boolean;
+  size?: "lg" | "md";
+}
+
+// TODO: icon 추가해서 테스트 하기
+const Badge = React.forwardRef<React.ElementRef<"span">, BadgeProps>((props, ref) => {
+  const { asChild, className, color, size = "md", ...badgeProps } = props;
+
+  const Comp = asChild ? Slot : "span";
+  return (
+    <Comp
+      data-accent-color={color}
+      {...badgeProps}
+      ref={ref}
+      className={cn(
+        "rt-reset",
+        "bb-badge",
+        `bb-badge-color-${color}`,
+        `bb-badge-size-${size}`,
+        { "bb-label1_normal": size === "lg" },
+        { "bb-caption1": size === "md" },
+
+        className,
+      )}
+    />
+  );
+});
+Badge.displayName = "Badge";
 
 export { Badge };
+export type { BadgeProps };
